4866ff6f3cdbf61df96c962f97a40ae2176a847b
Brad P <b.powles@warwickschool.org>
2018-01-31 12:18:57 +0000

    Item Object name change.

---
+++ Item_Objects/calculator.py
@@ -0,0 +1,13 @@
+import itemobjects
+
+
+class calculator:
+    users = ['Tom Bell']
+    def add(self):
+        number = 1
+        return number
+
+
+def setup():
+    itemobjects.programs.types.append(calculator)
+
--- Item_Objects/itemobjects.py
+++ Item_Objects/itemobjects.py
@@ -1,13 +1,13 @@
 import os
 read = []
 for file in os.listdir("."):
-    if file.endswith("here.py") and file not in read:
+    if file.endswith("or.py") and file not in read:
         exec("import " + file[:-3] + '\n' + file[:-3] + ".setup()")
     read.append(file)
 
 # mobs.json in the future
-class mobs:
+class programs:
     types = []
 # end
 
-print(mobs.types[0].weaknesses)
+print(programs.types[0].users)
