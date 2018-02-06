fb6dff235aca6a2ca6a35fe03d74395e1f0364f3
Brad P <b.powles@warwickschool.org>
2018-02-06 11:11:53 +0000

    Pre Release

--- GCSE - Pre Release/pre-release-testing-2018.py
+++ GCSE - Pre Release/pre-release-testing-2018.py
@@ -1,12 +1,49 @@
-tot = 0
+class Order:
+    def new(self, parts):
+        wholeprice = 0
+        order = []
+        for item in parts:
+            valid = False
+            while valid == False:
+                print(item[0])
+                for part in item:
+                    print("{}: £{} Remaining stock: {}".format(part[0], part[1], part[2]))
+                partchoice = input("{}: ".format(item[0]))
+                for u in range(1, len(item)):
+                    if partchoice == item[u][0]:
+                        if item[u][2] > 0:
+                            item[u][2] += -1
+                            valid = True
+                            wholeprice += item[u][1]
+                            order.append(item[u][0])
+                        else:
+                            print("None left")
 
-price_cpu = [100, 120, 200]
-type_cpu = ["p3", "p5", "p7"]
-input_cpu = input("What CPU? 1) p3  2) p5  3)p7")
-tot += price_cpu[(int(input_cpu) - 1)]
-input_cpu = type_cpu[(int(input_cpu) - 1)]
+                if valid == False:
+                    print("Not a valid order")
 
-print("Choices: {}".format(input_cpu))
-print(tot)
+        print("Order Finished")
+        print(order)
+        for i in range(len(parts)):
+            print(parts[i][0], order[i])
 
+        wholeprice = wholeprice * 120
+        wholeprice = int(wholeprice) / 100
+        print("Price is £", wholeprice, " (Including VAT 20%)", sep="")
+        print("")
+        input("Press enter to order another computer")
 
+class Menu:
+    def __init__(self):
+        self.parts = {
+            "Processor": [["p3", 100, 10], ["p5", 120, 10], ["p7", 80, 10]],
+            "RAM": [["16", 75, 10], ["32", 150, 10]],
+            "Storage": [["1TB", 50, 10], ["2TB", 100, 10]],
+            "Screen size": [["19", 65, 10], ["23", 120, 10]],
+            "Case": [["mini", 40, 10], ["midi", 70, 10]],
+            "USB ports": [["2", 10, 10], ["4", 20, 10]]
+        }
+    def neworder(self):
+        Order().new(self.parts)
+
+Menu().neworder()
