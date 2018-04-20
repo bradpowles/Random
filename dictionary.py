191e4b5a27f5b6a0caade4f62df9bac9f9e25523
Brad P <b.powles@warwickschool.org>
2018-04-20 15:03:35 +0100

    Updates to Style

--- dictionary.py
+++
@@ -1,9 +0,0 @@
-data = {'Monday': [],'Tuesday': [], 'Wednesday':[],'Thursday':[],'Friday':[],'Saturday':[],'Sunday':[]}
-for i in data:
-    data[i].append({})
-data['Monday'][-1]['Lat'] = 123456789
-print(data['Monday'])
-import datetime
-now = datetime.datetime.now()
-day = now.strftime("%A")
-print(data[day])
